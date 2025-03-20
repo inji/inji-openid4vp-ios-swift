4f362f79de92f8571339b2d4c3cfb537c70a272f
KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
2025-03-20 14:06:41 +0530

    [INJIMOB-2810] remove unused conditional downcast
    AuthorizationRequest encode function (which will be used to convert swift object to json), was checking if clientMetadata or presentationDefintion is string or ots respective object and then performing encode. This will not be necessary as presentationDefinition is of type PresentationDefinition and clientMetadata is of type ClientMetadata
    
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>

--- Sources/OpenID4VP/AuthorizationRequest/AuthorizationRequest.swift
+++ Sources/OpenID4VP/AuthorizationRequest/AuthorizationRequest.swift
@@ -31,22 +31,15 @@ public struct AuthorizationRequest : Encodable {
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(clientId, forKey: .client_id)
-        if let presentationDefString = presentationDefinition as? String {
-            try container.encode(presentationDefString, forKey: .presentation_definition)
-        } else if let presentationDefObject = presentationDefinition as? PresentationDefinition {
-            try container.encode(presentationDefObject, forKey: .presentation_definition)
-        }
+        try container.encode(presentationDefinition, forKey: .presentation_definition)
         try container.encode(responseType, forKey: .response_type)
         try container.encode(responseMode, forKey: .response_mode)
         try container.encode(nonce, forKey: .nonce)
         try container.encode(state, forKey: .state)
         try container.encode(responseUri, forKey: .response_uri)
         try container.encode(redirectUri, forKey: .redirect_uri)
-        if let clientMetadataString = clientMetadata as? String {
-            try container.encode(clientMetadataString, forKey: .client_metadata)
-        } else if let clientMetadataObject = clientMetadata as? ClientMetadata {
-            try container.encode(clientMetadataObject, forKey: .client_metadata)
-        }
+        try container.encode(clientMetadata, forKey: .client_metadata)
+        
     }
     
     static func validateAndCreateAuthorizationRequest(urlEncodedAuthorizationRequest: String, setResponseUri: @escaping (String) -> Void, shouldValidateClient: Bool, trustedVerifierJSON: [Verifier], networkManager: NetworkManaging) async throws -> AuthorizationRequest {
