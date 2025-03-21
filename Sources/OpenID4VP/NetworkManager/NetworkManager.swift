9abd61652173b0fb721451bd911cd305cdb9fe27
KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
2025-03-21 15:14:04 +0530

    [INJIMOB-2810] move constants to constants folder
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>

--- Sources/OpenID4VP/NetworkManager/NetworkManager.swift
+++ Sources/OpenID4VP/NetworkManager/NetworkManager.swift
@@ -54,22 +54,3 @@ public struct NetworkManager: NetworkManaging {
         }
     }
 }
-
-
-public enum HTTP_METHOD: String {
-    case POST
-    case GET
-}
-
-public typealias HttpMethod = HTTPMethod
-
-enum Header : String {
-    case contentType = "Content-Type"
-}
-
-
-public enum ContentTypes : String {
-    case applicationJson = "application/json"
-    case applicationJwt = "application/oauth-authz-req+jwt"
-    case applicationFormUrlEncoded = "application/x-www-form-urlencoded"
-}
