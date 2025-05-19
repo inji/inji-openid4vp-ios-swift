fc69f4fbb694beb4c23e9acd17c050ee46aac146
adityankannan-tw <adityan.kannan@thoughtworks.com>
2025-05-19 13:03:01 +0530

    [INJIMOB-fix]: update macOS version for spm package
    Signed-off-by: adityankannan-tw <adityan.kannan@thoughtworks.com>

--- Package.swift
+++ Package.swift
@@ -5,7 +5,8 @@ import PackageDescription
 let package = Package(
     name: "OpenID4VP",
     platforms: [
-        .iOS(.v14)
+        .iOS(.v14),
+        .macOS(.v12),
     ],
     products: [
         .library(
