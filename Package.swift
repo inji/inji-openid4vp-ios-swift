0e56e4641d442b56fba3b3a99e0d17f358a6aafb
balachandarg-tw <balachandar.g@thoughtworks.com>
2025-07-17 13:56:28 +0530

    [INJIMOB-3414]: Alamofire verrsion updated
    Signed-off-by: balachandarg-tw <balachandar.g@thoughtworks.com>

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/beatt83/jose-swift.git", "4.0.2"..<"4.0.3"),
-        .package(url: "https://github.com/Alamofire/Alamofire.git", "5.10.0"..<"5.10.1"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", "5.10.2"..<"5.10.3"),
         .package(url: "https://github.com/valpackett/SwiftCBOR.git",  "0.5.0"..<"0.5.1"),
         .package(url: "https://github.com/keefertaylor/Base58Swift.git", "2.1.7"..<"2.1.8")
     ],
