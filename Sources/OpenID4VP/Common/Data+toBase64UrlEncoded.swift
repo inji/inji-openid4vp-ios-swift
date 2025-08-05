576284ae31123626b4173b36cb281f79a5bd473e
KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
2025-08-05 20:02:08 +0530

    [INJIMOB-3409] refactor: rm unused function
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>

--- Sources/OpenID4VP/Common/Data+toBase64UrlEncoded.swift
+++ Sources/OpenID4VP/Common/Data+toBase64UrlEncoded.swift
@@ -13,21 +13,4 @@ extension Data {
     func toBase64UrlEncoded() -> String {
         return base64URLEscaped(self.base64EncodedString())
     }
-    
-    init?(hexString: String) {
-        let len = hexString.count / 2
-        var data = Data(capacity: len)
-        var index = hexString.startIndex
-        
-        for _ in 0..<len {
-            let nextIndex = hexString.index(index, offsetBy: 2)
-            guard nextIndex <= hexString.endIndex else { return nil }
-            let byteString = hexString[index..<nextIndex]
-            guard let byte = UInt8(byteString, radix: 16) else { return nil }
-            data.append(byte)
-            index = nextIndex
-        }
-        
-        self = data
-    }
 }
