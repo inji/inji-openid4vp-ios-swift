fdf689ce6ef4d4c446cb92f188c90128a6d2262e
KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
2025-09-18 12:05:27 +0530

    [INJIMOB-3539] refactor: add notes as comments
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>

--- Sources/OpenID4VP/AuthorizationResponse/CredentialInputDescriptorMapping.swift
+++ Sources/OpenID4VP/AuthorizationResponse/CredentialInputDescriptorMapping.swift
@@ -4,7 +4,11 @@ internal struct CredentialInputDescriptorMapping {
     let format: FormatType
     let credential: AnyCodable
     let inputDescriptorId: String
+    // Optional Identifier - unique identifier for the credential, used for mapping to unsignedVpToken to its related VPTokenSigningResult
+    // Example: UUID of the credential for SD-JWT, docType of the credential for mDoc
     var identifier: String?
+    // Optional nested path - Pointer to the location of the credential within a VP
+    // Example: for `ldp_vc` - "$.verifiableCredential[0]" -> the first credential in the verifiableCredential array of the VP Token contains the credential for the input descriptor id
     var nestedPath: String?
     
     init(format: FormatType, credential: AnyCodable, inputDescriptorId: String, identifier: String? = nil, nestedPath: String? = nil) {
