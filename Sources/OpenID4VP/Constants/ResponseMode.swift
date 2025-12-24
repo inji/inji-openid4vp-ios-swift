006aadbaa6b467694c3803a10b426c4e597ae17a
KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
2025-12-24 18:08:07 +0530

    [INJIMOB-3598] fix: response mode value for iar-post*
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>

--- Sources/OpenID4VP/Constants/ResponseMode.swift
+++ Sources/OpenID4VP/Constants/ResponseMode.swift
@@ -3,6 +3,6 @@ import Foundation
 public enum ResponseMode: String {
     case directPost = "direct_post"
     case directPostJwt = "direct_post.jwt"
-    case iarPost = "iar_post"
-    case iarPostJwt = "iar_post.jwt"
+    case iarPost = "iar-post"
+    case iarPostJwt = "iar-post.jwt"
 }
