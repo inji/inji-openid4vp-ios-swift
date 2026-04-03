be0c421dd49ed6cdc025dd2c1af91d4588adabe5
KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
2026-04-03 15:14:34 +0530

    add: spec version to trusted verifiers with default version as 1.0
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>

--- Sources/OpenID4VP/dto/Verifier.swift
+++ Sources/OpenID4VP/dto/Verifier.swift
@@ -5,11 +5,14 @@ public struct Verifier {
     public let responseUris: [String]
     public let jwksUri: String?
     public let allowUnsignedRequest: Bool
+    // specification version that the verifier supports, default is v1
+    public let specVersion: SpecVersion
 
-    public init(clientId: String, responseUris: [String], jwksUri: String? = nil, allowUnsignedRequest: Bool = false) {
+    public init(clientId: String, responseUris: [String], jwksUri: String? = nil, allowUnsignedRequest: Bool = false, specVersion: SpecVersion = .v1) {
         self.clientId = clientId
         self.responseUris = responseUris
         self.jwksUri = jwksUri
         self.allowUnsignedRequest = allowUnsignedRequest
+        self.specVersion = specVersion
     }
 }
