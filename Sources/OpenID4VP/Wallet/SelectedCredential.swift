14822ff2913000d2aa162d104060a5e1b1b45628
KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
2026-04-22 12:47:05 +0530

    refactor: add public init for public classes
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>

--- Sources/OpenID4VP/Wallet/SelectedCredential.swift
+++ Sources/OpenID4VP/Wallet/SelectedCredential.swift
@@ -4,10 +4,22 @@ public struct SelectedCredential: Codable {
     public let format: FormatType
     public let credential: AnyCodable
     public let credentialId: String
+
+    public init(format: FormatType, credential: AnyCodable, credentialId: String) {
+       self.format = format
+       self.credential = credential
+       self.credentialId = credentialId
+   }
 }
 
 public struct Credential : Codable {
     public let format: FormatType
     public let data: AnyCodable
     public let credentialId: String
+
+    public init(format: FormatType, data: AnyCodable, credentialId: String) {
+          self.format = format
+          self.data = data
+          self.credentialId = credentialId
+      }
 }
