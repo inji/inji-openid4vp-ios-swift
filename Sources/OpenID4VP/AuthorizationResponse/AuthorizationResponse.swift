303920a5d8df902eb2abb76f177408c8c6ad7a98
KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
2026-04-29 15:35:49 +0530

    fix: encoding logic og dcql vp token
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>

--- Sources/OpenID4VP/AuthorizationResponse/AuthorizationResponse.swift
+++ Sources/OpenID4VP/AuthorizationResponse/AuthorizationResponse.swift
@@ -14,14 +14,8 @@ enum AuthorizationResponse {
         
         switch self {
         case .dcql(let vpToken, let state):
-            var encodedVpToken: [String: String] = [:]
-            for (credentialQueryId, vpTokenValue) in vpToken {
-                let wrapped = vpTokenValue.map { AnyEncodable($0) }
-                encodedVpToken[credentialQueryId] = try encode(wrapped, fieldName: "vp_token", className: className)
-            }
-            let vpData = try JSONSerialization.data(withJSONObject: encodedVpToken)
-            
-            jsonEncodedAuthorizationResponse["vp_token"] = String(data: vpData, encoding: .utf8)
+            let encodableMap = vpToken.mapValues { tokens in tokens.map { AnyEncodable($0) } }
+            jsonEncodedAuthorizationResponse["vp_token"] = try encode(encodableMap, fieldName: "vp_token", className: className)
             if let state = state { jsonEncodedAuthorizationResponse["state"] = state }
             
         case .presentationExchange(let vpToken, let presentationSubmission, let state):
