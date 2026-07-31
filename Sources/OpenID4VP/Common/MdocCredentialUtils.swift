de40dc3bad7947d7b7c5f39290da77eaa43e7b58
KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>
2026-07-31 13:46:15 +0530

    refactor: handle dcql query evailation for mDoc
    Both mDoc structures of
    1. legacy where issuerSigned and docType are available as keys
    2. VCI 1.0 compliant VC where the issuerSigned is available
    
    Signed-off-by: KiruthikaJeyashankar <kiruthikavjshankar@gmail.com>

--- Sources/OpenID4VP/Common/MdocCredentialUtils.swift
+++ Sources/OpenID4VP/Common/MdocCredentialUtils.swift
@@ -76,3 +76,65 @@ func extractMdocDocType(from decodedCredential: CBOR, className: String) throws 
         )
     }
 }
+
+func getMdocDocTypeAndIssuerSigned(from credential: AnyCodable, className: String) throws -> (docType: CBOR, issuerSigned: CBOR) {
+    guard let mdocCredential = credential.value as? String else {
+        throw InvalidType(
+            message: "Invalid MSO-MDOC token: expected String",
+            className: className
+        )
+    }
+    guard let decodedCredential = try? decodeCBOR(base64EncodedInput: mdocCredential) else {
+        throw InvalidData( message: "Invalid Verifiable Credential: Error while decoding credential", className: className)
+    }
+    
+    if let issuerAuth = getValueFromCBORMap(cborMap: decodedCredential, key: "issuerAuth"){
+        switch issuerAuth {
+        case .array(let issuerAuthArray):
+            guard issuerAuthArray.count > 2,
+                  case let .byteString(msoBytes) = issuerAuthArray[2] else {
+                throw InvalidData(
+                    message: "issuerAuth payload missing or invalid",
+                    className: className
+                )
+            }
+            var decodedMso = try CBORDecoder(input: msoBytes).decodeItem()
+            if case let CBOR.tagged(tag, inner)? = decodedMso, tag.rawValue == 24 {
+                guard case let CBOR.byteString(innerBytes) = inner else {
+                    throw InvalidData(
+                        message: "cbor tag 24 inner not bstr",
+                        className: className
+                    )
+                }
+                decodedMso = try CBORDecoder(input: innerBytes).decodeItem()
+            }
+            guard let decodedMso = decodedMso else {
+                throw InvalidData(
+                    message: "Failed to decode MSO",
+                    className: className
+                )
+            }
+            if let docType = getValueFromCBORMap(cborMap: decodedMso, key: "docType"),
+               let docTypeString = extractStringFromCBOR(docType) {
+                return (docType, decodedCredential)
+            } else {
+                throw InvalidData(
+                    message: "docType missing or invalid in issuerAuth",
+                    className: className
+                )
+            }
+        default:
+            throw InvalidData(
+                message: "issuerAuth is not an array",
+                className: className
+            )
+        }
+    } else  if let docType = getValueFromCBORMap(cborMap: decodedCredential, key: "docType") {
+        return (docType, getValueFromCBORMap(cborMap: decodedCredential, key: "issuerSigned") ?? .null)
+    } else {
+        throw InvalidData(
+            message: "docType missing or invalid in credential",
+            className: className
+        )
+    }
+}
